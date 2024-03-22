4b464329d5a43a2aed0fe8fd572721f4c0ba4b8f
Angelina Zabalett <116826508+angieezabalett@users.noreply.github.com>
2024-03-22 12:49:05 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,15 +1,14 @@
 def change():
     expense = 23.75
     money = 100
-    print ("Ingresar gasto: ")
+    print ("Ingresar gasto:")
     print (expense)
-    print ("Dinero recibido ")
+    print ("Dinero recibido")
     print (money)
     print ("\nVuelto")
     print ("\nPesos:")
     resta = int (money - expense)
     print (resta)
-    print ("Centavos: ")
+    print ("Centavos:")
     cent = (money - expense)
     print (int ((cent - resta) * 100))
-change ()
